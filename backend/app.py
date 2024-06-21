f58b9b975f0430c2035382ad44e081040cc3af07
Shavidika <shavidika.ekanayake@gmail.com>
2024-06-22 02:38:13 +0530

    Changed the model

--- backend/app.py
+++ backend/app.py
@@ -8,20 +8,20 @@ app = Flask(__name__)
 CORS(app)
 
 # Load the trained model
-model = tf.keras.models.load_model('model.h5')
+model = tf.keras.models.load_model('next_word_prediction_model.h5')
 
 # Load the tokenizer
-with open('tokenizer.json') as f:
+with open('tokenizer_next_word_predict.json') as f:
     tokenizer_json = f.read()
     tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(tokenizer_json)
 
-max_length = 50  # Set this to the max_length used during training
+max_length = 19 
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.json
     input_text = data['input_text']
-    for _ in range(1):  # Generate 6 words as an example
+    for _ in range(1): 
         token_list = tokenizer.texts_to_sequences([input_text])[0]
         token_list = tf.keras.preprocessing.sequence.pad_sequences([token_list], maxlen=max_length-1, padding='pre')
         predicted = np.argmax(model.predict(token_list), axis=-1)
