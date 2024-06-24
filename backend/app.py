587a2e339bac70bcce910467aeb20a926c65ef72
Bawantha Rathnayake <Bawantharathnayake395@gmail.com>
2024-06-25 00:45:13 +0530

    final

--- backend/app.py
+++ backend/app.py
@@ -8,14 +8,14 @@ app = Flask(__name__)
 CORS(app)
 
 # Load the trained model
-model = tf.keras.models.load_model('next_word_prediction_model_news_headline_grp33.h5')
+model = tf.keras.models.load_model('next_word_prediction_model_news_headline_group_project.h5')
 
 # Load the tokenizer
-with open('tokenizer_news_headline_grp33.json') as f:
+with open('tokenizer_news_headline_group_project.json') as f:
     tokenizer_json = f.read()
     tokenizer = tf.keras.preprocessing.text.tokenizer_from_json(tokenizer_json)
 
-max_length = 18
+max_length = 19
 
 @app.route('/predict', methods=['POST'])
 def predict():
